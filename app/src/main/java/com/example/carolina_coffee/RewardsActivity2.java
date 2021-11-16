b8c67fbc6bbe35513f75edb7ce82925dd135cfbe
Christopher Wynne <chw5225@gmail.com>
2021-11-16 18:18:50 -0500

    RewardsActivity2 modifications for Westen.

--- app/src/main/java/com/example/carolina_coffee/RewardsActivity2.java
+++ app/src/main/java/com/example/carolina_coffee/RewardsActivity2.java
@@ -1,20 +1,40 @@
 package com.example.carolina_coffee;
 
-import android.app.Activity;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class RewardsActivity2 extends Activity {
-    String rewards;
-    String details;
-    int rewardPoints;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
-    public String getRewards() {
-        return rewards;
+public class RewardsActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        // Get rid of the top "Carolina_Coffee" purple bar on top of each page.
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM);
+        getSupportActionBar().hide(); // This line will hide the action bar
+
+        // This will change the action bar color from the default purple, to color of choice here.
+        // Calling to method that will make this action happen.
+        statusBarColor();
+
+        //This must be placed below the method above that changes/deletes action bar.
+        setContentView(R.layout.activity_rewards2);
+
     }
-    public String getDetails() {
-        return details;
-    }
-    public int getRewardPoints(){
-        return rewardPoints;
+
+    // This is method to change the status bar color from default purple to color of choice.
+    private void statusBarColor() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getWindow().setStatusBarColor(getResources().getColor(R.color.black,this.getTheme()));
+        }else if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            getWindow().setStatusBarColor(getResources().getColor(R.color.black));
+        }
     }
 
 }
