7b6e340264515dfc6ea7c4ad199c4bd74cd9e432
Westen Wingard <wingardw@email.uscb.edu>
2021-11-16 12:27:13 -0500

    reward class

---
+++ app/src/main/java/com/example/carolina_coffee/RewardsActivity2.java
@@ -0,0 +1,20 @@
+package com.example.carolina_coffee;
+
+import android.app.Activity;
+
+public class RewardsActivity2 extends Activity {
+    String rewards;
+    String details;
+    int rewardPoints;
+
+    public String getRewards() {
+        return rewards;
+    }
+    public String getDetails() {
+        return details;
+    }
+    public int getRewardPoints(){
+        return rewardPoints;
+    }
+
+}
