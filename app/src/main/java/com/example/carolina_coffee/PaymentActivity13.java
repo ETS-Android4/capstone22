126748ad851433bac5a52b367f9b061238559447
Christopher Wynne <chw5225@gmail.com>
2021-11-16 21:24:37 -0500

    -Update to PaymentActivity13.java to delete purple bar

--- app/src/main/java/com/example/carolina_coffee/PaymentActivity13.java
+++ app/src/main/java/com/example/carolina_coffee/PaymentActivity13.java
@@ -2,7 +2,10 @@ package com.example.carolina_coffee;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -16,6 +19,16 @@ public class PaymentActivity13 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Get rid of the top "Carolina_Coffee" purple bar on top of each page.
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM);
+        getSupportActionBar().hide(); // This line will hide the action bar
+
+        // This will change the action bar color from the default purple, to color of choice here.
+        // Calling to method that will make this action happen.
+        statusBarColor();
+
         setContentView(R.layout.activity_payment13);
 
         CardForm cardForm   = (CardForm) findViewById(R.id.card_form);
@@ -33,4 +46,14 @@ public class PaymentActivity13 extends AppCompatActivity {
             }
         });
     }
+
+    // This is method to change the status bar color from default purple to color of choice.
+    private void statusBarColor() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getWindow().setStatusBarColor(getResources().getColor(R.color.black,this.getTheme()));
+        }else if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            getWindow().setStatusBarColor(getResources().getColor(R.color.black));
+        }
+    }
+
 }
