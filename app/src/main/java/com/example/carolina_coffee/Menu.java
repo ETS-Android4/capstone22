7ccf4326ad4c5a939d8d143c28eb36e8c2198625
Caleb Howard <howardc484@gmail.com>
2021-11-17 22:18:20 -0500

    Developing menu portion - pending

---
+++ app/src/main/java/com/example/carolina_coffee/Menu.java
@@ -0,0 +1,65 @@
+package com.example.carolina_coffee;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Get rid of the top "Carolina_Coffee" purple bar on top of each page.
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM);
+        getSupportActionBar().hide(); // This line will hide the action bar
+
+
+        // Navigation
+        //--------------------------------------------------------------------------------------
+        //Initialize and assign variables - bottom_navigation = nav bar inside activity_main.xml
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        //Set home Selected
+        bottomNavigationView.setSelectedItemId(R.id.accountPageButton);
+        //perform itemSelectedListener
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    //Home Page Button
+                    case R.id.homePageButton:
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+
+                    //Payment Page Button
+                    case R.id.payPageButton:
+                        startActivity(new Intent(getApplicationContext(), PaymentActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+
+                    //Order Page Button
+                    case R.id.orderPageButton:
+                        startActivity(new Intent(getApplicationContext(), RewardsActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+
+                    //Account Page Button
+                    case R.id.accountPageButton:
+                        return true;
+                }
+                return false;
+            }
+        });
+        // End of Navigation
+        //--------------------------------------------------------------------------------------
+    }
+}
