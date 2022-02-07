b49230423c07320dcb3eeebc6c2e17424fc8978c
Christopher Wynne <chw5225@gmail.com>
2022-02-06 21:10:27 -0500

    -working on firebase increment

--- app/src/main/java/com/example/carolina_coffee/CartPageActivity.java
+++ app/src/main/java/com/example/carolina_coffee/CartPageActivity.java
@@ -1,6 +1,7 @@
 package com.example.carolina_coffee;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -23,17 +24,25 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ServerValue;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class CartPageActivity extends AppCompatActivity {
 
+    private static final String SCORE = "Score";
     RecyclerView recyler_menu;
     RecyclerView.LayoutManager layoutManager;
 
@@ -246,7 +255,7 @@ public class CartPageActivity extends AppCompatActivity {
 
             // Fire Base Increment for tracking rewards.
             //-----------------------------------------
-            //addIncrementRewardsToFireBase();
+            addIncrementRewardsToFireBase();
             //-----------------------------------------
 
             cart.getCart().clear();
@@ -268,6 +277,16 @@ public class CartPageActivity extends AppCompatActivity {
 
         userID = fAuth.getCurrentUser().getUid();
         DocumentReference documentReference = fStore.collection("rewards_increment").document(userID);
+
+
+        // Needs to read existing data from firebase
+        int increment = 0;
+
+        Map<String,Integer> user = new HashMap<>();
+        user.put("Increment", increment+5 );
+
+        
+
 
         documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
             @Override
