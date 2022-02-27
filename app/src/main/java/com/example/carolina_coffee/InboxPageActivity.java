9db73271afcd5c220567ec6d181681870806b869
Christopher Wynne <chw5225@gmail.com>
2022-02-26 21:31:21 -0500

    -fixed inbox page from crashing

--- app/src/main/java/com/example/carolina_coffee/InboxPageActivity.java
+++ app/src/main/java/com/example/carolina_coffee/InboxPageActivity.java
@@ -32,8 +32,23 @@ ArrayList<MessageData> list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //------------------------
+        // Comes first to get rid of the white default loading screen
+        // Eseentially this is wokring to bypass and remove default white screen that pops up when loading app.
+        // May change this later. ALSO This is also linking to Themes.xml's under Res/Values/themes.
+        setTheme(R.style.Theme_Carolina_Coffee);
+
+        // Get rid of the top "Carolina_Coffee" purple bar on top of each page.
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM);
+        getSupportActionBar().hide(); // This line will hide the action bar
+        // This will change the action bar color from the default purple, to color of choice here.
+        // Calling to method that will make this action happen.
+        statusBarColor();
+
         setContentView(R.layout.activity_inbox_page);
 
+/*
         recyclerView = findViewById(R.id.messageList);
         database = FirebaseDatabase.getInstance().getReference("Capstone/Message");
         recyclerView.setHasFixedSize(true);
@@ -52,7 +67,7 @@ ArrayList<MessageData> list;
                     list.add(message);
                 }
                 if(list.size()>0)
-                 myAdapter.notifyItemRangeInserted(0,list.size());
+                    myAdapter.notifyItemRangeInserted(0,list.size());
             }
 
 
@@ -62,30 +77,7 @@ ArrayList<MessageData> list;
             }
         });
 
-
-
-
-
-
-
-        //------------------------
-        // Comes first to get rid of the white default loading screen
-        // Eseentially this is wokring to bypass and remove default white screen that pops up when loading app.
-        // May change this later. ALSO This is also linking to Themes.xml's under Res/Values/themes.
-        setTheme(R.style.Theme_Carolina_Coffee);
-        //------------------------
-
-
-        // Get rid of the top "Carolina_Coffee" purple bar on top of each page.
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM);
-        getSupportActionBar().hide(); // This line will hide the action bar
-        // This will change the action bar color from the default purple, to color of choice here.
-        // Calling to method that will make this action happen.
-        statusBarColor();
-
-        setContentView(R.layout.activity_inbox_page);
-
+ */
 
 /*
 //Issue with this code, keeps crashing.
